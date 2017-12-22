52a9358fbacfa02e1bc2da1e116dcd49810bd2eb
Stephen Haywood <stephen@averagesecurityguy.info>
2017-12-22 12:28:28 -0500

    Add dataPath config option.

--- config.go
+++ config.go
@@ -13,6 +13,7 @@ type Keyword struct {
 type Config struct {
 	keys     map[string]time.Time
 	keywords []*Keyword
+	dataPath string
 	maxSize  int           // Do not save files larger than this.
 	maxTime  time.Duration // Max time to store previously downloaded keys.
 	sleep    time.Duration // Time to wait between each run.
@@ -25,6 +26,7 @@ func newConfig() Config {
 	c.maxSize = 1024 * 1024
 	c.maxTime = 3600 * time.Second
 	c.sleep = 60 * time.Second
+	c.dataPath = "data"
 
 	// Build our includes list.
 	c.keywords = loadKeywords()
