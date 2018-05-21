4009a220aa2407726aa129464f0692e21cafe527
Stephen Haywood <stephen@averagesecurityguy.info>
2018-05-20 21:10:19 -0400

    Replace search binary with kv.

--- build.sh
+++ build.sh
@@ -7,13 +7,12 @@ GO_BIN=go
 # Build our binary
 GOPATH=/tmp/go "$GO_BIN" get github.com/asggo/store
 GOPATH=/tmp/go "$GO_BIN" build -o bin/scrape
-GOPATH=/tmp/go "$GO_BIN" build -o bin/search ./search/
+GOPATH=/tmp/go "$GO_BIN" build -o bin/kv github.com/asggo/store/src
 
 # Stop Service
 sudo service scrape stop
 
 cp bin/scrape ${INSTALL_DIR}
-cp bin/search ${INSTALL_DIR}
-
+cp bin/kv ${INSTALL_DIR}
 # Start Service
 sudo service scrape start
