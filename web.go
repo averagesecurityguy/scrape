a453371a91fe13a5543f13d448c04a6864752cb0
Stephen Haywood <stephen@averagesecurityguy.info>
2018-09-17 01:10:33 -0400

    Additional html functions. Begin template conversion.

--- web.go
+++ web.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,29 +13,47 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type DataSet struct {
+	Bucket string
+	Batch map[string]string
+	Next string
+}
+
+func NewDataSet(bucket string) *DataSet {
+	d := new(DataSet)
+
+	d.Bucket = bucket
+	d.Batch = make(map[string]string)
+
+	return d
+}
+
+
 var header []byte
 var footer []byte
 var beginList = []byte("<ul>\n")
 var endList = []byte("</ul>\n")
 
+func escape(str string) string {
+	return html.EscapeString(str)
+}
+
 func bucketLi(item string) []byte {
+	item = escape(item)
+
 	return []byte(fmt.Sprintf("<li><a href=\"/keys/%s\">%s</a></li>\n", item, item))
 }
 
-func keyLi(bucket, key string) []byte {
-	return []byte(fmt.Sprintf("<li><a href=\"/read/%s/%s\">%s</a></li>\n", bucket, key, key))
-}
-
-func valLi(val string) []byte {
-	return []byte(fmt.Sprintf("<li>%s</li>\n", val))
-}
-
 func pre(data string) []byte {
+	data = escape(data)
+
 	return []byte(fmt.Sprintf("<pre>\n%s\n</pre>\n", data))
 }
 
-func heading(str string) []byte {
-	return []byte(fmt.Sprintf("<h2>%s</h2>\n", str))
+func heading(header string) []byte {
+	header = escape(header)
+
+	return []byte(fmt.Sprintf("<h2>%s</h2>\n", header))
 }
 
 // buckets  Returns a list of buckets.
@@ -59,47 +79,65 @@ func read(w http.ResponseWriter, r *http.Request) {
 
 	w.Write(header)
 	w.Write(heading(fmt.Sprintf("%s - %s", vars["bucket"], vars["key"])))
-	w.Write(pre(db.Read(vars["bucket"], vars["key"])))
+	w.Write(pre(escape(db.Read(vars["bucket"], vars["key"]))))
 	w.Write(footer)
 }
 
 func keys(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
+	ds := NewDataSet(vars["bucket"])
+	ds.Next = vars["next"]
 
-	w.Write(header)
-	w.Write(beginList)
-	w.Write(heading(fmt.Sprintf("Keys - %s", vars["bucket"])))
-	db.WalkBucket(vars["bucket"], func(key, val string) {
-		w.Write(keyLi(vars["bucket"], key))
-	})
-	w.Write(endList)
-	w.Write(footer)
+	err := db.ReadBatch(ds, 100)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	t, err := template.ParseFiles("web/templates/layout.html", "web/templates/keys.html")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	t.ExecuteTemplate(w, "layout", ds)
 }
 
 func vals(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
+	ds := NewDataSet(vars["bucket"])
+	ds.Next = vars["next"]
 
-	w.Write(header)
-	w.Write(beginList)
-	w.Write(heading(fmt.Sprintf("Values - %s", vars["bucket"])))
-	db.WalkBucket(vars["bucket"], func(key, val string) {
-		w.Write([]byte(valLi(val)))
-	})
-	w.Write(endList)
-	w.Write(footer)
+	err := db.ReadBatch(ds, 100)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	t, err := template.ParseFiles("web/templates/layout.html", "web/templates/vals.html")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	t.ExecuteTemplate(w, "layout", ds)
 }
 
 func search(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
 	w.Write(header)
+	w.Write(heading(fmt.Sprintf("Searching %s for %s", vars["bucket"], vars["term"])))
 	w.Write(beginList)
-	w.Write(heading("Search Results"))
-	db.WalkBucket(vars["bucket"], func(key, val string) {
+	err := db.WalkBucket(vars["bucket"], func(key, val string) {
 		if strings.Contains(val, vars["term"]) {
 			w.Write(keyLi(vars["bucket"], key))
 		}
 	})
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
 	w.Write(endList)
 	w.Write(footer)
 }
@@ -111,10 +149,11 @@ func startWebServer() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", buckets)
 	r.HandleFunc("/keys/{bucket}", keys)
+	r.HandleFunc("/keys/{bucket}/{next}", keys)
 	r.HandleFunc("/vals/{bucket}", vals)
 	r.HandleFunc("/read/{bucket}/{key}", read)
 	r.HandleFunc("/search/{bucket}/{term}", search)
-
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
 	srv := &http.Server{
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 30 * time.Second,
