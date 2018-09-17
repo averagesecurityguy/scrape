9b9d170f10e271a4e4220684a4bc3f082d631d72
AverageSecurityGuy <stephen@averagesecurityguy.info>
2018-09-17 13:06:32 -0400

    Update build to build view binary.

--- build.sh
+++ build.sh
@@ -5,14 +5,15 @@ INSTALL_DIR=/opt/scrape
 GO_BIN=go
 
 # Build our binary
-GOPATH=/tmp/go "$GO_BIN" get -u github.com/asggo/store
+GOPATH=/tmp/go "$GO_BIN" get -u github.com/boltdb/bolt
+GOPATH=/tmp/go "$GO_BIN" get -u github.com/gorilla/mux
 GOPATH=/tmp/go "$GO_BIN" build -o bin/scrape
-GOPATH=/tmp/go "$GO_BIN" build -o bin/kv github.com/asggo/store/src
+GOPATH=/tmp/go "$GO_BIN" build -o bin/view view/view.go view/store.go
 
 # Stop Service
 sudo service scrape stop
 
 cp bin/scrape ${INSTALL_DIR}
-cp bin/kv ${INSTALL_DIR}
+cp bin/view ${INSTALL_DIR}
 # Start Service
 sudo service scrape start
